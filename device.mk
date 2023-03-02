f8c10e260d495e851a8fadb90313fbe1ed0e4f9d
Woomymy <woomy@woomy.be>
2023-03-02 20:57:04 +0100

    LH7n: Build bootctrl into vendor
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: Icdc56ceab2fe3b54c8538574d406dd4ce1f0a5de
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -5,6 +5,8 @@
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-mtkimpl \
     android.hardware.boot@1.2-mtkimpl.recovery
 
 # Init files
