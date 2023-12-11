cb9a3eb3083ba2a9501a2f47b049834fb05c023c
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-12-11 09:06:56 +0700

    Add keylayout file to map double tap event to wake
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -150,6 +150,10 @@ PRODUCT_PACKAGES += \
     init.audio.samsung.rc \
     ueventd.mt6789.rc
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/mtk-tpd.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/mtk-tpd.kl
+
 # Keymaster
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
