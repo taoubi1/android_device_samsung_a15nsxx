07645c87ac31d38c2d848cf21e5f8a13cac44f85
1bdellatif <ptaoubi@gmail.com>
2025-07-03 20:58:43 +0100

    Revert "mt6789-common: Inherit virtual ab launch_with_vendor_ramdisk makefile"
    This reverts commit 88e83ca2d4c3b15d86a0cdfce4c208a081f47bce.

--- common.mk
+++ common.mk
@@ -7,7 +7,6 @@
 COMMON_PATH := device/tecno/mt6789-common
 
 # A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
 
 AB_OTA_UPDATER := true
