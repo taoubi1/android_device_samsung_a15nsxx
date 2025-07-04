67682fedc4458f0c119af80a5d89cd5235bcd99b
1bdellatif <taoubiabdellatif2025@gmail.com>
2025-07-04 20:38:11 +0100

    Configures kernel and modules for vendor ramdisk
    Configures the kernel build process to copy the kernel image
    to the correct location.
    
    Defines variables for kernel modules, including those for
    recovery and vendor dlkm, to ensure they are loaded correctly
    into the vendor ramdisk. This includes reading module lists from
    files and using wildcards to include all .ko files in the vendor_dlkm directory.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,9 +87,30 @@ BOARD_RECOVERY_MKBOOTIMG_ARGS := \
   --dtb $(DEVICE_PATH)/prebuilt/dtb.img
 
 # Prebuilt kernel and DTBO
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+
+LOCAL_KERNEL := $(KERNEL_PATH)/Image.gz
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# Workaround to make lineage's soong generator work
+TARGET_KERNEL_SOURCE := device/samsung/a15nsxx-kernel/kernel-headers
+
+# Kernel modules
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/ramdisk/modules.load))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(KERNEL_PATH)/ramdisk/, $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD))
+
+# Also add recovery modules to vendor ramdisk
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/ramdisk/modules.load.recovery))
+RECOVERY_MODULES := $(addprefix $(KERNEL_PATH)/ramdisk/, $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD))
+
+# Prevent duplicated entries (to solve duplicated build rules problem)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(sort $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES) $(RECOVERY_MODULES))
+
+# Vendor modules (installed to vendor_dlkm)
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_dlkm/modules.load))
+BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(KERNEL_PATH)/vendor_dlkm/*.ko)
 
 # Broken Rules
 BUILD_BROKEN_DUP_RULES := true
