0a568e6513ffab59ea8e9f663a640f300d52ace4
Shirayuki39 <lorddemecrius83@proton.me>
2025-07-29 13:31:02 +0900

    LH7n: Initial Skeleton Tree
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_LH7n.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_LH7n-user \
+    lineage_LH7n-userdebug \
+    lineage_LH7n-eng
---
+++ lineage_LH7n.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/tecno/LH7n/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+BOARD_VENDOR := TECNO
+PRODUCT_NAME := lineage_LH7n
+PRODUCT_DEVICE := LH7n
+PRODUCT_MANUFACTURER := TECNO
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO LH7n
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DeviceName=LH7n
+    BuildFingerprint=TECNO/LH7n-GL/TECNO-LH7n:14/UP1A.231005.007/250416V781:user/release-keys
