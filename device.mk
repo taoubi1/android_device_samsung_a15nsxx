7adb9ab2251a21da5aece5686072595d8a51f171
1bdellatif <taoubiabdellatif2025@gmail.com>
2025-08-08 21:29:31 +0100

    Configure Bluetooth Stack

--- device.mk
+++ device.mk
@@ -51,6 +51,14 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+PRODUCT_PACKAGES += \
+   vendor.mediatek.hardware.bluetooth.audio@2.1.vendor:64 \
+   vendor.mediatek.hardware.bluetooth.audio@2.2.vendor:64
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor:64
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
