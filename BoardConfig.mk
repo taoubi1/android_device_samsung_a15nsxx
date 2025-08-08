40a297d30d66623939eae24dd2ca75a5932f3a16
1bdellatif <taoubiabdellatif2025@gmail.com>
2025-08-08 19:54:16 +0100

    a15nsxx: BoardConfig: Define Bootloader and Platform flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,11 +6,18 @@
 DEVICE_PATH := device/samsung/a15nsxx
 CONFIGS_PATH := $(DEVICE_PATH)/configs
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := a15
+TARGET_NO_BOOTLOADER := true
+
 # Display
 TARGET_SCREEN_DENSITY := 450
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := a15nsxx,a15
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6789
 
 # Properties
 TARGET_PRODUCT_PROP += $(CONFIGS_PATH)/properties/product.prop
