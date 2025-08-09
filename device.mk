06f9609a32952fad4e7059639f2903374bc609ef
1bdellatif <taoubiabdellatif2025@gmail.com>
2025-08-09 17:01:18 +0100

    a15nsxx: Add initial partitions configuration

--- device.mk
+++ device.mk
@@ -14,6 +14,9 @@ TARGET_SCREEN_WIDTH := 1080
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 34
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
