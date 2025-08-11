bba595ee43edf01d4e564a0f6ef3f26ed24959fa
1bdellatif <taoubiabdellatif2025@gmail.com>
2025-08-11 01:34:34 +0100

    a15nsxx: Configure Display Stack

--- device.mk
+++ device.mk
@@ -14,6 +14,15 @@ TARGET_SCREEN_WIDTH := 1080
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 34
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.3-service \
+    android.hardware.memtrack-service.mediatek-mali \
+    android.hardware.graphics.allocator@4.0.vendor:64 \
+    android.hardware.graphics.mapper@4.0.vendor:64 \
+    libhwc2on1adapter:64 \
+    libhwc2onfbadapter:64
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
