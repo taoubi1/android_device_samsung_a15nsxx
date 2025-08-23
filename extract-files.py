d1ebc5436db5f969e544ff854ee2d296e4617563
1bdellatif <taoubiabdellatif2025@gmail.com>
2025-08-23 11:09:06 +0100

    Import nfc

--- extract-files.py
+++ extract-files.py
@@ -4,19 +4,26 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.file import File
 from extract_utils.fixups_blob import (
+    BlobFixupCtx,
     blob_fixup,
     blob_fixups_user_type,
 )
-
 from extract_utils.fixups_lib import (
+    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
-
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
+)
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
 )
 
 namespace_imports = [
@@ -31,11 +38,38 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 
 
 lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
 }
+
+
+def blob_fixup_return_1(
+    ctx: BlobFixupCtx,
+    file: File,
+    file_path: str,
+    symbol: str,
+    *args,
+    **kwargs,
+):
+    for line in run_cmd(
+        [
+            llvm_objdump_path,
+            '--dynamic-syms',
+            file_path,
+        ]
+    ).splitlines():
+        if line.endswith(f' {symbol}'):
+            offset, _ = line.split(maxsplit=1)
+
+            with open(file_path, 'rb+') as f:
+                f.seek(int(offset, 16))
+                f.write(b'\x01\x00\xa0\xe3')  # mov r0, #1
+                f.write(b'\x1e\xff\x2f\xe1')  # bx lr
+
+            break
+
 
 blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
-
 
 module = ExtractUtilsModule(
     'a15nsxx',
@@ -43,7 +77,6 @@ module = ExtractUtilsModule(
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
-    check_elf=False,
 )
 
 if __name__ == '__main__':
